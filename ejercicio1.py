125bf1c34cb0188f493dcbe44c3ea764ed5af891
Yessenia1 <84026815+Yessenia1@users.noreply.github.com>
2022-05-01 17:22:22 -0500

    Delete ejercicio1.py

--- ejercicio1.py
+++
@@ -1,8 +0,0 @@
-# Realice un diagrama de flujo y pseudocódigo que representen el algoritmo para obtener el área de un triángulo.
-base:float
-altura:float
-area:float
-base=int(input("ingrese la base: "))
-altura=int(input("ingrese la altura: "))
-area=base*altura
-print(f"el area del triangulo es:{area}")
