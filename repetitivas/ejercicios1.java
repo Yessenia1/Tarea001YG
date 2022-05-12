a03925d092da663bcaa69d22f9aa31077c711bea
Yessenia1 <alejandraredfg3@gmail.com>
2022-05-12 17:54:43 +0000

    xd

--- repetitivas/ejercicios1.java
+++ repetitivas/ejercicios1.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
+
 /**
  * ejercicios1
  */
 public class ejercicios1 {
+    
 
     
         public static void whiile() {
-            int numInit=21;
+            int numInit=1;
             while (numInit<=20){
                 System.out.println(numInit);
                 numInit=numInit+1;
@@ -15,16 +18,38 @@ public class ejercicios1 {
         
         }
         public static void dowhile() {
-            int numInit=21;
+            int numInit=1;
             do{
                 System.out.println(numInit);
                 numInit++;
             }while (numInit<=20);
             
         }
-
+        public static void forw() {
+            
+            for (int numInit=1; numInit<=20; numInit=numInit++) {
+                System.out.println(numInit);
+                
+              }
+            
+        }
+        public static void nombref() {
+            Scanner lt=new Scanner(System.in);
+            System.out.println("ingrese su nombre");
+            String nombre=lt.next();
+            
+            for (int i = nombre.length()-1; i >=0; i--) {
+                
+                System.out.println(nombre.charAt(i));
+            }
+           
+        
+            
+        }
     public static void main(String[] args) {  
-        System.out.println("imprime while: "); whiile();
+        /*System.out.println("imprime while: "); whiile();
         System.out.println("imprime do while"); dowhile();
+        System.out.println("imprimir for"); forw();*/
+        nombref();
     }
 }
