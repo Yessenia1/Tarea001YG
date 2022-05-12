6afa45a97ad1c9b74d04a1f127a06e546dfdb673
Yessenia1 <alejandraredfg3@gmail.com>
2022-05-12 03:37:21 +0000

    .

--- examen01/ejercicio3.py
+++ examen01/ejercicio3.py
@@ -6,7 +6,7 @@ pregunta: float
 a=float(input("introduzca un numero: "))
 b=float(input("introduzca otro número: "))
 pregunta=float(input("Introduzca la operación matematica (+)=1,(-)=2,(*)=3,(/)=4" +
-"(^ potencia)=5,(R raiz cuadrada)=6,(% modulo de dos)=7 favor de poner como respuesta solo el numero porque todavia nose como se igual sin numeros ;("))
+"n\(^ potencia)=5,(R raiz cuadrada)=6,(% modulo de dos)=7 n\/favor de poner como respuesta solo el numero porque todavia nose como se igual sin numeros ;(/ : "))
 if pregunta<=1:
     c=a+b
 elif pregunta<=2:
@@ -16,7 +16,7 @@ elif pregunta<=3:
 elif pregunta<=4:
     c=a/b
 elif pregunta<=5:
-    c=a*a
+    c=pow(a,b)
 elif pregunta<=6:
     c=math.sqrt(a)
 elif pregunta<=7:
