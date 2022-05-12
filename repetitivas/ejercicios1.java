5a67e23004949bcaf9b0e8b05771b0d0311f69ae
Yessenia1 <alejandraredfg3@gmail.com>
2022-05-12 16:07:21 +0000

    12/05

---
+++ repetitivas/ejercicios1.java
@@ -0,0 +1,21 @@
+/**
+ * ejercicios1
+ */
+public class ejercicios1 {
+
+    
+        public static void whiile() {
+            int numInit=1;
+            while (numInit<=20){
+                System.out.println(numInit);
+                numInit=numInit+1;
+
+            
+            }
+        
+        }
+
+    public static void main(String[] args) {  
+        whiile();
+    }
+}
