adb256780fd1f06b12d1fd250f9d3c5b4674a9f3
Yessenia1 <alejandraredfg3@gmail.com>
2022-05-12 16:27:41 +0000

    11:27

--- repetitivas/ejercicios1.java
+++ repetitivas/ejercicios1.java
@@ -5,7 +5,7 @@ public class ejercicios1 {
 
     
         public static void whiile() {
-            int numInit=1;
+            int numInit=21;
             while (numInit<=20){
                 System.out.println(numInit);
                 numInit=numInit+1;
@@ -14,8 +14,17 @@ public class ejercicios1 {
             }
         
         }
+        public static void dowhile() {
+            int numInit=21;
+            do{
+                System.out.println(numInit);
+                numInit++;
+            }while (numInit<=20);
+            
+        }
 
     public static void main(String[] args) {  
-        whiile();
+        System.out.println("imprime while: "); whiile();
+        System.out.println("imprime do while"); dowhile();
     }
 }
