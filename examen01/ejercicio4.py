786d30266bda302009e2ccbd4dd710e1b233457d
Yessenia1 <alejandraredfg3@gmail.com>
2022-05-13 01:18:41 +0000

    xd

--- examen01/ejercicio4.py
+++ examen01/ejercicio4.py
@@ -1,14 +1,14 @@
 edad: int
-genero: int
+genero: str
 vacuna: str
 edad=int(input("¿cuantos años tiene?"))
-genero=int(input("usted es hombre=1 o mujer=2 /porfavor introduzca el numerito/ : "))
+genero=str(input("usted es hombre o mujer : "))
 if edad>70:
     vacuna=(f"La vacuna que le toca es el tipo C")
 elif edad>16 and edad<=59:
-    if genero<=1:
+    if genero=="hombre":
         vacuna=(f"la vacuna que le toca es el tipo B")
-    elif genero<=2:
+    elif genero=="mujer":
         vacuna=(f"la vacuna que le toca es el tipo A")
 elif edad<16:
     vacuna=(f"la vacuna que le toca es el tipo A")
