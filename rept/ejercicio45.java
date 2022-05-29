b7254aca223a11e0ca2cafca0c0da58b19b92d47
Yessenia1 <alejandraredfg3@gmail.com>
2022-05-29 20:11:55 +0000

    .

---
+++ rept/ejercicio45.java
@@ -0,0 +1,44 @@
+package rept;
+
+
+public class ejercicio45 {
+    //4.1.	Un profesor tiene un salario inicial de S/ 1500, 
+    //y recibe un incremento de 10 % anual durante 6 años. ¿Cuál es su salario al cabo de 6 años? 
+    //¿Qué salario ha recibido en cada uno de los 6 años? 
+    //Realice el algoritmo utilizando el ciclo apropiado.
+    public static void main(String [] args){
+        //por for
+        int salario = 1500;
+        double r =0;
+        for(int i =0; i<=6;i++){
+          r = salario+(salario*0.10)*i;
+          System.out.print("el salario al cabo de "+i);
+          System.out.println(" años es "+r);
+        }
+
+        //por while
+        int salario1 =1500;
+        double r1=0;
+        int i =0;
+        while (i <= 6) {
+            r1 = salario1+(salario1*0.10)*i;
+            System.out.print("el salario al cabo de "+i);
+            System.out.println(" años es "+r1);
+            i++;
+        }
+
+        //do while
+        int salario2 =1500;
+        double r2=0;
+        i=0;
+        
+        do {
+            r2 = salario2+(salario2*0.10)*i;
+            System.out.print("el salario al cabo de "+i);
+            System.out.println(" años es "+r2);
+           i++;
+        }
+        while (i <= 6);
+        
+    }
+}
