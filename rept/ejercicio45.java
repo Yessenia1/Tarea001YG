a6503eba6ebc0a74202c62885b4a9470f106cdd0
Yessenia1 <alejandraredfg3@gmail.com>
2022-05-29 18:38:55 +0000

    .

--- rept/ejercicio45.java
+++ rept/ejercicio45.java
@@ -10,8 +10,44 @@ public class ejercicio45 {
     // de lo contrario, sólo se le aplica 10%. Se debe saber cuál es el costo 
     //y el descuento que tendrá cada uno de los artículos y finalmente cuánto se pagará por todos los artículos obtenidos. 
     //Represente la solución mediante el diagrama de lujo, el pseudocódigo y el diagrama N/S. 
-    int n;
-    System.out.print("cuanto es el costo del articulo");
+    
+    public static void main(String[] args){
+        int x;
+        float n;
+        double d;
+        float p;
+        System.out.print("¿Cuantos articulos compro? ");
+        x=lt.nextInt();
+        for(int i=1; i<=x; i++){
+            System.out.print("cuanto es el costo del articulo: ");
+            n=lt.nextFloat();
+            if(n>=200){
+              p= 15;
+              d = n-(n*(0.15));
+              System.out.println("El descuento del articulo: "+p);
+              System.out.println("%");
+              System.out.println("El costo total: "+d);
+
+            }else if(n>100 && n<200){
+              d = n-(n*(0.12));
+              p = 12;
+              System.out.print("El descuento del articulo: "+p);
+              System.out.println("%");
+              System.out.println("El costo total: "+d);
+
+            }else if(n<=100){
+              d = n-(n*(0.10));
+              p=10;
+              System.out.print("El descuento del articulo: "+p);
+              System.out.println("%");
+              System.out.println("El costo total: "+d);
+            }
+            
+        }
+        
+        
+    }
+    
 
 
 }
